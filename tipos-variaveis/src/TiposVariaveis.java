b5398b8ebe89c832392047be78d69d271ef1314b
adson.trindade <adson.santos101@outlook.com>
2024-10-21 21:38:24 -0300

    novo exemplo de codigos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        String apresentação = "Meu nome é Adson Trindade, nascido em ";
+        int anoNascimento = 1999;
+        String complemento = "Estou aprendendo desenvlvimento de software na linguagem java";
+
+        System.out.println(apresentação + anoNascimento);
+        System.out.print(complemento);
+        
     }
-}
+    
+}    
