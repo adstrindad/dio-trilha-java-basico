35bd83bad0a8bab2610030ee91792e2b410ce75e
adson.trindade <adson.santos101@outlook.com>
2024-10-23 22:07:55 -0300

    exemploTiposVariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,13 +1,33 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) {
-        String apresentação = "Meu nome é Adson Trindade, nascido em ";
-        int anoNascimento = 1999;
-        String complemento = "Estou aprendendo desenvlvimento de software na linguagem java";
-
-        System.out.println(apresentação + anoNascimento);
-        System.out.print(complemento);
         
+        String caractere = "Adson Trindade"; 
+        // variavel usada para declarar sequencia de caracteres.
+        System.out.println(caractere);
+
+        int numerosInteiros = 100; 
+        // variavel usada para declarar numero de valor inteiro.
+        System.out.println(numerosInteiros);
+
+        double numerosFracionados = 3.14; 
+        // varivel usada para declarar numero de valor decimal.
+        System.out.println(numerosFracionados);
+
+        boolean logico = true; 
+        // variavel usada para declarar valor logico (verdadeiro/falso).
+        System.out.println(logico);
+
+        char caractereUnica = 'V'; 
+        // variavel usada para armazenar uma unica caractere.
+        System.out.println(caractereUnica);
+
+        final int anoNascimento = 1999; 
+        // usada para armazenar valores que não se alteram.
+        System.out.println(anoNascimento);
+
+        String descrição = "Se observa acima exemplos de algumas das variaveis mais utilizadas na linguagem java e suas respectivas descrições";
+        System.out.print(descrição);
     }
     
 }    
